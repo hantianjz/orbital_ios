821d614e81d3ca47f4877101e0f06f8e37225951
James Zhao <hantian@google.com>
2022-10-28 14:25:46 -0400

    Initial Commit

---
+++ Orbital/OrbitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitalApp.swift
+//  Orbital
+//
+//  Created by James Zhao on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct OrbitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
